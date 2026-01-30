c286720034f612a811ae3ef172a7c3fe7c750334
Md Mohibbullah <mohammedhanif74612@gmail.com>
2026-01-30 14:50:22 +0200

    fix: export

---
+++ src/api/services.js
@@ -0,0 +1,15 @@
+import { api } from "./client";
+
+export async function getServices() {
+  const { data } = await api.get("/api/services");
+  return data.services;
+}
+
+export async function createService(payload) {
+  const { data } = await api.post("/api/services", payload);
+  return data.service;
+}
+
+export async function deleteService(id) {
+  await api.delete(`/api/services/${id}`);
+}
